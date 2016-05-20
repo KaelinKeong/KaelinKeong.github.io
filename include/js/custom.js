4533d0cf2c4e2be02222bf07755956760c42ffe4
KaelinKeong <kaelin.keong@gmail.com>
2016-05-20 14:50:02 -0700

    E-Portfolio
    Homepage for E-Portfolio

---
+++ include/js/custom.js
@@ -0,0 +1,61 @@
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            /*====================================
+             CUSTOM LINKS SCROLLING FUNCTION 
+            ======================================*/
+
+            $('a[href*=#]').click(function () {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+               && location.hostname == this.hostname) {
+                    var $target = $(this.hash);
+                    $target = $target.length && $target
+                    || $('[name=' + this.hash.slice(1) + ']');
+                    if ($target.length) {
+                        var targetOffset = $target.offset().top;
+                        $('html,body')
+                        .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+                        return false;
+                    }
+                }
+            });
+            /*====================================
+            VAGAS SLIDESHOW SCRIPTS
+            ======================================*/
+            $(function () {
+                $.vegas('slideshow', {
+                    backgrounds: [
+                      { src: 'include/img/Malaysia.jpg', fade: 1000, delay: 9000 }, 
+                      
+                     
+                    ]
+                })('overlay', {
+                    /** SLIDESHOW OVERLAY IMAGE **/
+                    src: 'include/plugins/vegas/overlays/14.png'
+                });
+
+            });
+
+
+
+          
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+
