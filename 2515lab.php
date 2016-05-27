3bd30de15e7469f8e792c79f9750f709558cb123
KaelinKeong <kaelin.keong@gmail.com>
2016-05-27 13:30:48 -0700

    Add files via upload

---
+++ 2515lab.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>ACIT 2515 Lab</title>
+    <!--REQUIRED STYLE SHEETS-->
+    <!-- BOOTSTRAP CORE STYLE CSS -->
+    <link href="include/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLE CSS -->
+    <link href="include/css/font-awesome.min.css" rel="stylesheet" />
+    <!-- VEGAS STYLE CSS -->
+    <link href="include/scripts/vegas/jquery.vegas.min.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE CSS -->
+    <link href="include/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Ruluko' rel='stylesheet' type='text/css' />
+
+    
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+         <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Java Lab</a>
+            </div>
+                    <li><a href="#"><strong>e-mail:</strong>  kaelin.keong@gmail.com</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!--End Navigation -->
+    
+<section class="for-full-back color-white" id="about">
+        <div class="container">    
+    
+<div class="row text-left">
+                <div class="col-md-8 col-md-offset-2">
+                    <h4>
+                        <strong>public class DogTest {
+<br>
+    public static void main(String[] args) {
+        <br>
+        Labrador labrador = new Labrador("Rudy", "yellow");
+        System.out.println(labrador.getName() + " says " + labrador.speak());
+        <br>
+        Yorkshire yorkshire = new Yorkshire("Max");
+        System.out.println(yorkshire.getName() + " says " + yorkshire.speak());
+        <br>
+        KillerWolf killerwolf = new KillerWolf ("Rex");
+        System.out.println(killerwolf.getName() + " says " + killerwolf.speak());
+        <br>
+        System.out.println("The average breed weight for a labrador dog is " 
+        + labrador.avgBreedWeight());
+                            <br>
+        System.out.println("The average breed weight for a yorkshire dog is " 
+        + yorkshire.avgBreedWeight());
+                            <br>
+        System.out.println("The average breed weight for a killer wolf is " 
+        + killerwolf.avgBreedWeight());
+        <br>
+    }
+                            <br>
+} 
+</strong>
+                    </h4>
+                    
+                    <div class="row text-center">
+                                <a href="https://github.com/KaelinKeong/KaelinKeong.github.io/tree/master/javalab5" class="btn btn-default">FULL JAVA CODE</a>
+                        <a href="https://github.com/KaelinKeong/KaelinKeong.github.io/blob/master/Assignment%234.mp4?raw=true" class="btn btn-default">SCREENCAST FOR JAVA LAB</a>
+
+                </div>
+                </div>
+            </div>
+    </div>
